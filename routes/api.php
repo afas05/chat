c25e86aa17d28dbe0a85cceae3d4e63d8debd0cb
lampdev <lamdev@lamp-dev.com>
2019-11-24 13:41:10 +0200

    files

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group(['middleware' => 'jwt.auth'], function(){
+    Route::post('auth/login', 'AuthController@login');
+    Route::post('auth/logout', 'AuthController@logout');
+
+});
+
+Route::post('/registration', 'Auth\RegisterController@addNewUser');
